34795b517a0a5ecfe7c2545fe028a95abef4f8d0
leonardobfn <nascimento@ufam.edu.br>
2024-01-11 10:47:28 -0300

    atualização

---
+++ Atividades/06-programacao_funcional.R
@@ -0,0 +1,53 @@
+
+
+
+# Questão 1 ---------------------------------------------------------------
+
+#' Considere a lista abaixo
+#' Use map para contar quantos caracteres tem cada palavra
+palavras <- list("gato", "cachorro", "elefante", "leão")
+
+
+# Questão 2 ---------------------------------------------------------------
+
+#' Crie um json com as seguintes variáveis: produto,preço e vencimento
+#' Considere 3 produtos
+#' Selecione o segundo argumento
+
+
+# Questão 3 ---------------------------------------------------------------
+#' O código a seguir simula o desempenho de um teste t para dados normais. 
+#' Repita o teste N vezes usando map
+#' Extraia o valor p de cada teste.
+t.test( rnorm(100, 10), rnorm(90, 10) )
+
+
+# Questão 4 ---------------------------------------------------------------
+#' Reproduza o exemplo abaixo usando snowfall
+n = 200
+x = rnorm(n,30,3) # covariável
+b0 = 2
+b1 = 1
+y  = b0 + b1*x + rnorm(n) # resposta simulada
+
+dados = data.frame(y,x)
+rs = lm(y~x,data = dados) # aplicando modelo de regressão
+
+B = 100 # quantidade de repetições
+results = matrix(0,B,2)
+for(i in 1:B){
+  id_B = sample(1:n,n,replace = T)
+  dados_B = dados[id_B,]
+  rs_b = lm(y~x,data = dados_B)
+  COEF = coef(rs_b)
+  results[i,] = COEF
+}
+
+hist(results[,1])
+hist(results[,2])
+colMeans(results)
+
+
+# Questão 5 ---------------------------------------------------------------
+
+#' Elabore um exemplo para usar a função map2 ou pmap
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Curso de Introdução à Ciência de Dados
 
+-   [Aula_11-01-2024](https://rpubs.com/leonardobfn/Aula_11-01-2024)
 -   [Aula_19-12-2023](https://rpubs.com/leonardobfn/Aula_19-12-2023)
 -   [Aula_14-12-2023](https://rpubs.com/leonardobfn/Aula_14-12-2023)
 -   [Aula_12-12-2023](https://rpubs.com/leonardobfn/Aula_12-12-2023)
